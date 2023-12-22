55609769f1c49c100c7679477ad07dd4816d2ec9
Victor Osinaga <97135978+Victor-Osinaga@users.noreply.github.com>
2023-12-21 21:03:35 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const navbar = document.getElementById('navbar')
+
+window.addEventListener("scroll", function () {
+    window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+    window.scrollY < 100 && (navbar.style.background = `transparent`);
+  });
